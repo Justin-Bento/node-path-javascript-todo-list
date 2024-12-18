c34e86e1cea8a849d1438fdc7bc7e5ea65d27d28
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-17 20:30:01 -0400

    Added task-doing element.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 
 const title = document.querySelector("#item-title");
 const submission = document.querySelector(".item-post");
+const taskDoing = document.querySelector(".tasks-doing");
 
 document.addEventListener("DOMContentLoaded", () => {
   submission.addEventListener("click", (event) => {
