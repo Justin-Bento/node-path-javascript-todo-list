228c902ae319f586139a60587de54f37e3ef2348
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-17 20:22:10 -0400

    When a user clicks on the "Add Task" button it will send the final value to the console.

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ const submission = document.querySelector(".item-post");
 document.addEventListener("DOMContentLoaded", () => {
   submission.addEventListener("click", (event) => {
     event.preventDefault();
+    console.log(title.value);
   });
 });
