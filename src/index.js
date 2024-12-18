6224524eacc89694087966d461d62380bf3b873c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-17 20:20:31 -0400

    Fixed spelling mistake.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 const title = document.querySelector("#item-title");
-const submittion = document.querySelector(".item-post");
+const submission = document.querySelector(".item-post");
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log(title);
