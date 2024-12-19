82d4c1732e61502af18b6e0884497aace44353f9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-19 10:44:44 -0400

    deleted unneeded files.

--- src/delete.js
+++
@@ -1,6 +0,0 @@
-export const Delete = () => {
-  // Child Of Task Doing - Priority
-  const taskDoingChildAction = NewElement("button", "tdc-delete");
-  taskDoingChildAction.textContent = "Delete";
-  taskDoingChild2nd.appendChild(taskDoingChildAction);
-};
