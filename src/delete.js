64bdc10481ece1e2bebdcb5c742667b07f5cbc1f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-19 09:35:00 -0400

    Created a delete file to separate the logic between creating and deleting individual posts.

---
+++ src/delete.js
@@ -0,0 +1,6 @@
+export const Delete = () => {
+  // Child Of Task Doing - Priority
+  const taskDoingChildAction = NewElement("button", "tdc-delete");
+  taskDoingChildAction.textContent = "Delete";
+  taskDoingChild2nd.appendChild(taskDoingChildAction);
+};
